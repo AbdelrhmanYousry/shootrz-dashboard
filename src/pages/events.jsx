b269f02d306cae66713eaa75fc297cfc41c75b5b
AbdelrhmanYousry <eng_abdelrhman93@yahoo.com>
2020-09-08 11:11:57 +0200

    added package and category data to events

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -3,19 +3,25 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { rootURL } from "../utils/api";
 import { getCookie } from "../utils/cookie";
-function getEvents() {
+async function getEvents() {
   const token = getCookie("shootrz-dashboard-token");
-
- return  axios
-    .get(rootURL + "events", {
-      headers: {
-        Authorization: token,
-      },
-    })
-    .then((response) => { return response.data.events});
+  const response = await axios.get(rootURL + "events", {
+    headers: {
+      Authorization: token,
+    },
+  });
+  return response.data;
 }
 export default function Events() {
   const { data, isLoading, isError } = useQuery("events", getEvents);
+  let categoriesSet = {};
+  let packagesSet = {};
+  data && data.categories.forEach((el) => {
+    categoriesSet[el.id] = el;
+  });
+  data && data.packages.forEach((el) => {
+    packagesSet[el.id] = el;
+  });
   if (isError) {
     return (
       <div className="alert alert-danger mt-2" role="alert">
@@ -25,7 +31,7 @@ export default function Events() {
   }
   if (isLoading) {
     return (
-      <div className="d-flex justify-content-center">
+      <div className="d-flex justify-content-center mt-5">
         <div className="spinner-border" role="status">
           <span className="sr-only">Loading...</span>
         </div>
@@ -39,27 +45,38 @@ export default function Events() {
           <thead>
             <tr>
               <th scope="col">#</th>
-
               <th scope="col">Username</th>
               <th scope="col">Email</th>
               <th scope="col">Location</th>
               <th scope="col">Address</th>
               <th scope="col">Date</th>
               <th scope="col">Status</th>
+              <th scope="col">Category Name</th>
+              <th scope="col">Package Name</th>
+              <th scope="col">Package Price</th>
+              <th scope="col">Package Hours</th>
             </tr>
           </thead>
           <tbody>
-            {data && data.map((event, index) => (
-              <tr key={event.id}>
-                <th scope="row">{index + 1}</th>
-                <td>{event.user.name}</td>
-                <td>{event.user.email}</td>
-                <td>{event.location.name}</td>
-                <td>{event.address}</td>
-                <td>{event.date}</td>
-                <td>{event.status}</td>
-              </tr>
-            ))}
+            {data && data.events.map((event, index) => {
+              let pack = { ...packagesSet[event.package.package_id] };
+              let category = { ...categoriesSet[event.package.category_id] };
+              return (
+                <tr key={event.id}>
+                  <th scope="row">{index + 1}</th>
+                  <td>{event.user.name}</td>
+                  <td>{event.user.email}</td>
+                  <td>{event.location.name}</td>
+                  <td>{event.address}</td>
+                  <td>{event.date}</td>
+                  <td>{event.status}</td>
+                  <td>{category.name}</td>
+                  <td>{pack.name}</td>
+                  <td>{pack.price}</td>
+                  <td>{pack.hours}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
